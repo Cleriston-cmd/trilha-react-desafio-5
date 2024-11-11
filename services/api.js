8e0a180cd940a3e7958faa438561e9011fe14b96
Cleriston-cmd <c.andrade6533@gmail.com>
2024-11-11 11:49:50 -0300

    Implantando pagina de detalhes do post

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+export const api = axios.create({
+    baseURL: 'https://fendqnevuckngtsqlura.supabase.co/rest/v1/',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        'apikey': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlbmRxbmV2dWNrbmd0c3FsdXJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEzMzQyMjksImV4cCI6MjA0NjkxMDIyOX0.ki5qw8JmozXAAstqTfOeUnrXgaYX1PleHkNxru12tAw",
+        'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlbmRxbmV2dWNrbmd0c3FsdXJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEzMzQyMjksImV4cCI6MjA0NjkxMDIyOX0.ki5qw8JmozXAAstqTfOeUnrXgaYX1PleHkNxru12tAw"
     }
-})
+});
--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -12,8 +12,12 @@ export const getPosts = async () => {
 
 export const getPostBySlug = async (id) => {
 
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
+    //BUSCAR UM POST EM ESPECIFICO.
+    const {data} = await api.get(`/post?id=eq.${id}`);
 
-    return {}
+    if (data && data.length > 0) {
+        return data[0];
+    }
+
+    return null;
 }
